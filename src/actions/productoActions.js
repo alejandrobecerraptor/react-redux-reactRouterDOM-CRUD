58d1542636c3a99971818ef70dcb99c472d9ac4f
Alejandro <alejandro.becerra.pro@gmail.com>
2021-03-03 11:26:21 -0500

    configurando el actions , para una nueva consulta

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -1,7 +1,10 @@
 import {
     AGREGAR_PRODUCTO,
     AGREGAR_PRODUCTO_EXITO,
-    AGREGAR_PRODUCTO_ERROR
+    AGREGAR_PRODUCTO_ERROR,
+    COMENZAR_DESCARGA_PRODUCTOS,
+    DESCARGA_PRODUCTOS_EXITO,
+    DESCARGA_PRODUCTOS_ERROR
 } from '../types';
 import clienteAxios from '../config/axios';
 import Swal from 'sweetalert2';
@@ -49,8 +52,20 @@ const agregarProductoExito = producto =>({
 });
 
 // y si hubo un error
-
 const agregarProductoError = estado =>({
     type:AGREGAR_PRODUCTO_ERROR,
     payload: estado
 });
+
+//FUNCION QUE DESCARGA LOS PRODUCTOS DE LA BASE DE DATOS
+export function obtenerProductosAction(){
+    return async (dispatch) => {
+        dispatch(descargarProductos());
+    }
+}
+
+const descargarProductos = () => ({
+    type: COMENZAR_DESCARGA_PRODUCTOS,
+    payload: true
+});
+
