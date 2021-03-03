72752628e74f82e2591415c13a526925abaaf024
Alejandro <alejandro.becerra.pro@gmail.com>
2021-03-03 13:08:10 -0500

    eliminando producto del state y de la api

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -97,11 +97,25 @@ export function borrarProductoAction(id){
     return async (dispatch) => {
         dispatch(obtenerProductoEliminar(id));
 
-        console.log(id)
+        try {
+            await clienteAxios.delete(`/productos/${id}`);
+            dispatch(eliminarProductoExito());
+        } catch (error) {
+            
+        }
     }
 }
 
 const obtenerProductoEliminar = id => ({
     type: OBTENER_PRODUCTO_ELIMINAR,
     payload: id
+})
+
+const eliminarProductoExito = () => ({
+    type: PRODUCTO_ELIMINADO_EXITO
+})
+
+const eliminarProductoError = () => ({
+    type: PRODUCTO_ELIMINADO_ERROR,
+    payload: true
 })
