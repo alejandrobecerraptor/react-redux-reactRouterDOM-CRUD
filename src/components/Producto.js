7c5f0fe6d2ea828b0fb8e2ca320bf78875cca072
Alejandro <alejandro.becerra.pro@gmail.com>
2021-03-04 09:01:36 -0500

    poniendo en activo la ruta de editar

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import {Link, useHistory} from 'react-router-dom'
 import Swal from 'sweetalert2';
 //redux
 import {useDispatch} from 'react-redux';
@@ -9,6 +9,7 @@ const Producto = ({producto}) => {
     const {nombre,precio,id} = producto;
 
     const dispatch = useDispatch();
+    const history = useHistory();
 
     //confirmar si desea eliminarlo
     const confirmarEliminarProducto = id =>{
@@ -30,14 +31,21 @@ const Producto = ({producto}) => {
           })
         
     }
+
+    //funcion que redirege de forma programada
+    const redireccionarEdicion = producto => {
+        history.push(`/productos/editar/${producto.id}`)
+    }
+
     return ( 
         <tr>
             <td>{nombre}</td>
             <td><span className="font-weight-bold">$ {precio}</span></td>
             <td className="acciones">
-                <Link to={`/productos/editar/${id}`} className="btn btn-primary mr-2">
+                <button 
+                    type="button" className="btn btn-primary mr-2" onClick={() => redireccionarEdicion(producto)}>
                     Editar
-                </Link>
+                </button>
                 <button type="button" className="btn btn-danger" onClick={() => confirmarEliminarProducto(id)}>
                     Eliminar
                 </button>
