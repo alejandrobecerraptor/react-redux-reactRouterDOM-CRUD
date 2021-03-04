dc7d709dbc3ff7ac065abb74cfaff199c81d5e92
Alejandro <alejandro.becerra.pro@gmail.com>
2021-03-04 09:52:10 -0500

    editando producto , y action error

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -150,18 +150,28 @@ const obtenerProductoAction = producto => ({
 
 export function editarProductoAction(producto){
     return async (dispatch) => {
-        dispatch( editarProducto(producto))
+        dispatch( editarProducto())
 
         try {
-            const resultado = await clienteAxios.put(`/productos/${producto.id}`, producto);
-            console.log(resultado)
+            await clienteAxios.put(`/productos/${producto.id}`, producto);
+           dispatch(editarProductoExito(producto));
         } catch (error) {
-            
+            console.log(error)
+            dispatch(editarProductoError());
         }
     }
 }
 
-const editarProducto = producto => ({
-    type: COMENZAR_EDICION_PRODUCTO,
+const editarProducto = () => ({
+    type: COMENZAR_EDICION_PRODUCTO
+});
+
+const editarProductoExito = producto =>({
+    type:PRODUCTO_EDITADO_EXITO,
     payload: producto
+});
+
+const editarProductoError = () =>({
+    type:PRODUCTO_EDITADO_ERROR,
+    payload: true
 });
